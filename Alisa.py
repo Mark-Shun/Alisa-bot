fccc1a89f9749b27841fa20716ab74e2ff9d54d6
Mark-Shun <markslingerland@live.nl>
2023-03-09 18:34:52 +0100

    Setup Alisa class system

--- Alisa.py
+++ Alisa.py
@@ -3,10 +3,23 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='.', intents=intents)
+intents.members = True
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+class Alisa(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-bot.run('MTA4MzEyMjQ1ODkxMzE0NDg4Mw.GU6Bw-.WaeOyTDm_D1Mgdv-xCBO7wKHTAro-Ld5R4rnF4')
+    async def on_ready(self):
+        print(f'Alisa has been booted up')
+
+    async def on_message(self, message):
+        # Alisa should not reply to herself
+        print(f'Noticed a message: ' + str(message.content))
+        if message.author.id == self.user.id:
+            return
+        if message.content.startswith('hello'):
+            await message.reply('Hi there!', mention_author=True)
+
+
+client = Alisa(command_prefix='.', intents=intents)
+client.run('MTA4MzEyMjQ1ODkxMzE0NDg4Mw.GU6Bw-.WaeOyTDm_D1Mgdv-xCBO7wKHTAro-Ld5R4rnF4')
