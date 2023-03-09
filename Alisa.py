d11504082d4b2731ba187da8efbc493300abb6a2
Mark-Shun <markslingerland@live.nl>
2023-03-09 18:41:25 +0100

    Setup logging feature

--- Alisa.py
+++ Alisa.py
@@ -1,5 +1,8 @@
 import discord
+import logging
 from discord.ext import commands
+
+handler = logging.FileHandler(filename='alisa.log', encoding='utf-8', mode='w')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -21,5 +24,5 @@ class Alisa(discord.Client):
             await message.reply('Hi there!', mention_author=True)
 
 
-client = Alisa(command_prefix='.', intents=intents)
+client = Alisa(command_prefix='.', intents=intents, log_handler=handler)
 client.run('MTA4MzEyMjQ1ODkxMzE0NDg4Mw.GU6Bw-.WaeOyTDm_D1Mgdv-xCBO7wKHTAro-Ld5R4rnF4')
