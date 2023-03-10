f3de33274393e25cce7b9418f4c28fda088a5418
Mark-Shun <markslingerland@live.nl>
2023-03-10 20:24:24 +0100

    Added available roles list, main/sub switch.

--- Alisa.py
+++ Alisa.py
@@ -28,13 +28,24 @@ def is_valid_role(role):
 class Alisa(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.guild = None
+        self.alisa_main = None
+        self.alisa_sub = None
 
     async def on_ready(self):
         bot_name = (str(self.user)[0:-5])
         print(f'{bot_name} is now awake.')
-        #await message.channel.send("Alisa has been booted up")
+        self.guild = bot.get_guild(config.GUILD_ID)
+        self.alisa_main = discord.utils.get(self.guild.roles, name="Alisa Main")
+        self.alisa_sub = discord.utils.get(self.guild.roles, name="Alisa Sub")
 
 bot = Alisa(command_prefix=config.PREFIX, intents=intents, log_handler=handler)
+
+# Error handler
+@bot.event
+async def on_command_error(ctx,error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.reply(f"That command is not recognized, use {config.PREFIX}help for guidance.", mention_author=True)
 
 # Role management
 @bot.command()
@@ -50,9 +61,21 @@ async def iam(ctx, *, role: discord.Role):
         if role_obj in ctx.author.roles:
             await ctx.channel.send(f"You already have the {role_obj.name} role.")
         else:
+            # Checking if character role gets changed, to eventually remove the other one
+            if role_obj == bot.alisa_main:
+                opp_role = bot.alisa_sub
+            elif role_obj == bot.alisa_sub:
+                opp_role = bot.alisa_main
+            else:
+                opp_role = None
+                
             try:
                 await ctx.author.add_roles(role_obj)
-                await ctx.channel.send(f"You now have the {role_obj.name} role!")
+                message = f"You now have the {role_obj.name} role!"
+                if opp_role and opp_role in ctx.author.roles:
+                    await ctx.author.remove_roles(opp_role)
+                    message += f"\n And I removed the {opp_role.name} role."
+                await ctx.channel.send(message)
             except discord.errors.Forbidden:
                 await ctx.send("I do not have permission to grant this role.")
     else:
@@ -67,7 +90,6 @@ async def iam_error(ctx,error):
 @commands.check(lambda ctx: ctx.channel.id == config.ROLES_CHANNEL)
 async def iamnot(ctx, *, role: discord.Role):
     #Check if the role exists
-    print("Role to check: " + str(role))
     guild = ctx.guild
     role_obj = discord.utils.get(guild.roles, name=role.name)
     if role_obj is None:
@@ -86,11 +108,24 @@ async def iamnot_error(ctx,error):
     if isinstance(error, commands.RoleNotFound):
         await ctx.send(f"Excuse me, but that role does not exist.")
 
+@bot.command(aliases=["lsar"])
+async def roles(ctx):
+    message = "These are the current roles that you can assign:"
+    message += "\n**Character roles:**\n- "
+    message += "\n- ".join(config.CHARACTER_ROLES)
+    message += "\n**Region roles:**\n- "
+    message += "\n- ".join(config.REGION_ROLES)
+    message += "\n**Platform roles:**\n- "
+    message += "\n- ".join(config.PLATFORM_ROLES)
+    message += "\n**Misc roles:**\n- "
+    message += "\n- ".join(config.MISC_ROLES)
+    await ctx.channel.send(message)
+
+
 # Replying to defined messages
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
-    print(f'Noticed a message: ' + str(message.content))
     if message.author == bot.user:
         return # Ignore Alisa's own messages
     
--- config.py
+++ config.py
@@ -2,11 +2,15 @@ MAIN_TOKEN = 'MTA4MzEyMjQ1ODkxMzE0NDg4Mw.Gb6MqV.XWf2KjP-3Sj75QIjMX9W_3SkJoWZ11-q
 TEST_TOKEN = 'MTA4MzM5NjE1NDQ2Njc2Mjc4Mg.GRQMjk.maxDv79YYhsIY6nnytvwodrxLnUqcEXipTOF0s'
 PREFIX = '.'
 
-CHARACTER_ROLES = ["Alisa Main", "Alisa Sub", "Guest"]
+CHARACTER_ROLES = ["Alisa Main", "Alisa Sub"]
 REGION_ROLES = ["NA-EAST", "NA-WEST", "EU-EAST", "EU-WEST", "OCE", "EAST-ASIA", "SEA", "MEA"]
 PLATFORM_ROLES = ["PC", "PS4", "XBOX"]
 MISC_ROLES = ["Guest", "Streamer"]
 
 VALID_ROLES = CHARACTER_ROLES + REGION_ROLES + PLATFORM_ROLES + MISC_ROLES
 
+# This is the ID for the roles channel, this is used to check if the role commands are used in the right channel
 ROLES_CHANNEL = 1083761874979532901
+
+# This is the ID of the guilder (server)
+GUILD_ID = 427866625899888640
