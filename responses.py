337ae13f0488f90db5b96e4dea2e0c1aca954d79
Mark-Shun <markslingerland@live.nl>
2023-03-10 23:20:43 +0100

    Added heart and love responses

--- responses.py
+++ responses.py
@@ -4,13 +4,14 @@ import discord
 class Responses:
     def __init__(self, bot):
         self.bot = bot
-        #self.alisa_notes = bot.get_emoji(826896985826918430)
         self.alisa_notes = discord.utils.get(self.bot.emojis, name='AlisaNotes')
         self.alisa_dancing = discord.utils.get(self.bot.emojis, name='AlisaDancing')
         self.alisa_baila = discord.utils.get(self.bot.emojis, name='AlisaBaila')
         self.alisa_pout = discord.utils.get(self.bot.emojis, name='AlisaPout')
         self.arisa = discord.utils.get(self.bot.emojis, name="arisabig")
         self.alisa_cry = discord.utils.get(self.bot.emojis, name="alisacry")
+        self.heart = discord.utils.get(self.bot.emojis, name="Heart")
+        self.alisa_happy = discord.utils.get(self.bot.emojis, name="Alisa_Happy")
 
 
     async def handle_message(self, message):
@@ -29,5 +30,9 @@ class Responses:
             await message.channel.send(self.alisa_cry)
         if "alisa bible" == p_message:
             await message.channel.send("Find the Alisa Bible here: https://docs.google.com/document/d/1hntdSiK3CBprurfSRCm5mKmc7hsGxBwA2ILQZBX2l8Y/edit?usp=sharing")
+        if "heart" == p_message:
+            await message.channel.send(self.heart)
+        if "i love alisa" == p_message:
+            await message.reply(f'I love you too {self.alisa_happy}')
         
         
