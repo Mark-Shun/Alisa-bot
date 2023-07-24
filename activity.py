496a59cbcc1e5166d8453a36fd23a65f3c419afe
Mark-Shun <57092443+Mark-Shun@users.noreply.github.com>
2023-07-24 18:43:07 +0200

    Typo fix

--- activity.py
+++ activity.py
@@ -1,12 +1,11 @@
 import discord
-import warnings
 import random
 
 activities = [
     discord.Game(name="TEKKEN 6"),
     discord.Game(name="TEKKEN TAG TOURNAMENT 2"),
     discord.Game(name="TEKKEN 7"),
-    discord.Activity(type=discord.ActivityType.competing,name="Iron Fist Tournament"),
+    discord.Activity(type=discord.ActivityType.competing,name="King of Iron Fist Tournament"),
     discord.Activity(type=discord.ActivityType.watching,name="Gajimaru guides"),
     discord.Streaming(name="Alisa Whiff Tutorial", url="https://www.youtube.com/watch?v=7dgtXQiP7J4&t=47s")
 ]
